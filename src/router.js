33834c3117a040660c50a4d1a682756d19030cb8
Michael Meier <mjmeier8@gmail.com>
2018-11-28 19:45:42 -0600

    Added login and logout features in frontend

--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import Router from "vue-router";
 import Home from "./views/Home.vue";
 import About from "./views/About.vue";
 import Signup from "./views/Signup.vue";
+import Login from "./views/Login.vue";
+import Logout from "./views/Logout.vue";
 
 Vue.use(Router);
 
@@ -10,6 +12,8 @@ export default new Router({
   routes: [
     { path: "/", name: "home", component: Home },
     { path: "/about", name: "about", component: About },
-    { path: "/signup", name: "signup", component: Signup }
+    { path: "/signup", name: "signup", component: Signup },
+    { path: "/login", name: "login", component: Login },
+    { path: "/logout", name: "logout", component: Logout }
   ]
 });
