0e35f8e85c2841f2988e8b235dccdcecf17659b1
Michael Meier <mjmeier8@gmail.com>
2018-12-04 14:19:24 -0600

    show games from backend, added bootstrap checkboxes and dropdowns

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> | <router-link to="/signup">Signup</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/logout">Logout</router-link>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" <router-link to="/">Home</router-link>  |
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" <router-link to="/signup">Signup</router-link> | <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item"><a class="nav-link" <router-link to="/login">Login</router-link> | </a></li>
+            <li class="nav-item"><a class="nav-link" router-link to="/logout">Logout</router-link> </a></li>
+          </ul>
+        </div>
+      </nav>
     </div>
     <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,53 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm">
+          <p>Home Team</p>
+          <div v-for="game in games">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value="" id="defaultCheck1" />
+              <label class="form-check-label" for="defaultCheck1"> Make Pick </label>
+            </div>
+
+            <h2>{{ game.home_team }}</h2>
+            <div class="form-group">
+              <label for="exampleFormControlSelect1">Example select</label>
+              <select class="form-control" id="exampleFormControlSelect1">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </select>
+            </div>
+            <p>score {{ game.home_score }}</p>
+          </div>
+        </div>
+        <div class="col-sm">
+          <p>Away Team</p>
+          <div v-for="game in games">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value="" id="defaultCheck1" />
+              <label class="form-check-label" for="defaultCheck1"> Make Pick </label>
+            </div>
+            <h2>{{ game.away_team }}</h2>
+            <div class="form-group">
+              <label for="exampleFormControlSelect1">Example select</label>
+              <select class="form-control" id="exampleFormControlSelect1">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </select>
+            </div>
+            <p>score {{ game.away_score }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,10 +59,18 @@ var axios = require("axios");
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "NBA Confidence Pick'em League",
+      games: []
     };
   },
-  created: function() {},
+  created: function() {
+    axios.get("http://localhost:3000/api/games").then(
+      function(response) {
+        console.log(response.data);
+        this.games = response.data;
+      }.bind(this)
+    );
+  },
   methods: {},
   computed: {}
 };
