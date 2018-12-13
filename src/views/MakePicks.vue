00676ac9da8ac3d3e2d7d4f03a16921be07661f8
Michael Meier <mjmeier8@gmail.com>
2018-12-12 21:26:24 -0600

    updated drag and drop for making picks so that highest point is always at top

--- src/views/MakePicks.vue
+++ src/views/MakePicks.vue
@@ -51,20 +51,25 @@
     </div> -->
 
     <h1>{{ message }}</h1>
-    <div class="container">
+    <div class="kf_result_thumb">
       <form v-on:submit.prevent="savePicks();">
-        <draggable v-model="games" @start="drag=true" @end="drag=false">         
+        <!-- <draggable v-model="games" @start="drag=true" @end="drag=false">          -->
+        <draggable v-model="games" @end="finishDrag()">         
           <div v-for="game in games" class="row">
               <span v-on:click="game.game_id = game.id;"></span>
               <div class="col-md-2"><input type="number" v-model="game.confidence_point" /></div>
               <div class="col-md-5">
-                <h4 v-on:click="game.users_pick = game.home_nickname;">{{ game.home_nickname }}</h4>
+                <div class="figure pull-left">
+                  <h4 v-on:click="game.users_pick = game.home_nickname;">{{ game.home_nickname }}</h4>
+                </div>
               </div>
               <div class="col-md-5">
-                <h4 v-on:click="game.users_pick = game.visitor_nickname;">{{ game.visitor_nickname }}</h4>
+                <div class="figure pull-right">
+                  <h4 v-on:click="game.users_pick = game.visitor_nickname;">{{ game.visitor_nickname }}</h4>
+                </div>
               </div>
               <p>You chose {{ game.users_pick }}</p>
-            </div>
+          </div>
         </draggable>
         <input type="submit" value="Save picks" />
       </form>
@@ -106,6 +111,13 @@ export default {
     );
   },
   methods: {
+    finishDrag: function() {
+      var cp = this.games.length;
+      this.games.forEach(game => {
+        game.confidence_point = cp;
+        cp--;
+      });
+    },
     savePicks: function() {
       console.log("savePicks");
       this.games.forEach(game => {
